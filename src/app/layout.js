3548b9f3e2b4a84c559dec84c35d6b7b6534fedb
crack69-git <tanchangyaashutosh0356@gmail.com>
2026-06-18 18:22:50 +0600

    added navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavbarUser from "@/Components/Shared/NavbarUser";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,9 +21,13 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
+      data-theme="Light"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <NavbarUser></NavbarUser>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
